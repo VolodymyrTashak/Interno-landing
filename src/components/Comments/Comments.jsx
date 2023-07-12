69ab506d960b2ad7e56d27093791fc7ad112066b
VolodymyrTashak <vovavozny33@gmail.com>
2023-07-12 12:08:43 +0300

    update

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,4 +1,7 @@
-import { CommentsBox, CommentsTitle, CommentsList, CommentsItem } from "./Comments.styled";
+import { CommentsBox, CommentsTitle, CommentsList, CommentsItem, CommentsWrapper, UserBox, UserTitle, UserText, CommentsText } from "./Comments.styled";
+import user1 from "../../img/Lisa Clairton.webp"
+import user2 from "../../img/David Knight.webp"
+import user3 from "../../img/Lisa Knight.webp"
 
 export const Comments = () => {
     return (
@@ -6,34 +9,34 @@ export const Comments = () => {
         <CommentsTitle>What the People Thinks About Interno</CommentsTitle>
         <CommentsList>
             <CommentsItem>
-                <div>
-                    <img />
-                    <div>
-                        <h3>Lisa Clairton</h3>
-                        <p>New York, USA</p>
-                    </div>
-                </div>
-                <p>We selected Interno interior because of rigorous design background & education.</p>
+                <CommentsWrapper>
+                    <img src={user1} alt="Lisa Clairton" width={77} height={77} />
+                    <UserBox>
+                        <UserTitle>Lisa Clairton</UserTitle>
+                        <UserText>New York, USA</UserText>
+                    </UserBox>
+                </CommentsWrapper>
+                <CommentsText>We selected Interno interior because of rigorous design background & education.</CommentsText>
             </CommentsItem>
             <CommentsItem>
-                <div>
-                    <img />
-                    <div>
-                        <h3>David Knight</h3>
-                        <p>Sydney, Australia</p>
-                    </div>
-                </div>
-                <p>The team exceeded our expectations and emerged as leaders of our project’s.</p>
+                <CommentsWrapper>
+                <img src={user2} alt="David Knight" width={77} height={77} />
+                    <UserBox>
+                        <UserTitle>David Knight</UserTitle>
+                        <UserText>Sydney, Australia</UserText>
+                    </UserBox>
+                </CommentsWrapper>
+                <CommentsText>The team exceeded our expectations and emerged as leaders of our project’s.</CommentsText>
             </CommentsItem>
             <CommentsItem>
-                <div>
-                    <img />
-                    <div>
-                        <h3>Lisa Clairton</h3>
-                        <p>Paris, France</p>
-                    </div>
-                </div>
-                <p>They balanced the architectural vision and the project realities beautifully.</p>
+                <CommentsWrapper>
+                <img src={user3} alt="Lisa Knight" width={77} height={77} />
+                    <UserBox>
+                        <UserTitle>Lisa Knight</UserTitle>
+                        <UserText>Paris, France</UserText>
+                    </UserBox>
+                </CommentsWrapper>
+                <CommentsText>They balanced the architectural vision and the project realities beautifully.</CommentsText>
             </CommentsItem>
         </CommentsList>
       </CommentsBox>
--- src/components/Comments/Comments.styled.jsx
+++ src/components/Comments/Comments.styled.jsx
@@ -30,3 +30,40 @@ padding: 60px 20px;
 background-color: #fff;
 border-radius: 25px;
 `;
+
+export const CommentsWrapper = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+margin-bottom: 20px;
+`;
+
+export const UserBox = styled.div`
+margin-left: 30px;
+`;
+
+export const UserTitle = styled.h3`
+font-family: DM Sans,sans-serif;
+font-size: 25px;
+font-style: italic;
+font-weight: 600;
+margin-bottom: 5px;
+`;
+
+export const UserText = styled.p`
+color: #4D5053;
+font-family: DM Sans,sans-serif;
+font-size: 22px;
+font-style: normal;
+font-weight: 500;
+line-height: 1.5;
+`;
+
+export const CommentsText = styled.p`
+color: #4D5053;
+font-family: DM Sans,sans-serif;
+font-size: 25px;
+font-style: normal;
+font-weight: 500;
+line-height: 1.5;
+`;
